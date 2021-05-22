fb863700d2480dd77ce0ef3dedcd83ed269457fb
t170046 <49540960+t170046@users.noreply.github.com>
2021-05-23 00:34:56 +0900

    Initial Commit

---
+++ Beacon2/Beacon2App.swift
@@ -0,0 +1,17 @@
+//
+//  Beacon2App.swift
+//  Beacon2
+//
+//  Created by 高井 on 2021/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Beacon2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beacon2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Beacon2
+//
+//  Created by 高井 on 2021/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
